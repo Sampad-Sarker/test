96b2e3e42f9d0468e065ffce582c31c90061d984
sampad-sarker <sampadsarker777@gmail.com>
2020-03-30 01:45:17 +0600

    from sublime

---
+++ greaterNum.js
@@ -0,0 +1,17 @@
+const a =5,b=7,c=9;
+
+if(a>b){
+  if(a>c){
+    console.log("a is big");
+  }  
+}
+else if(b>a){
+  if(b>c){
+    console.log("b is big");
+
+  }
+}
+else{
+  console.log("c is big");
+}
+
